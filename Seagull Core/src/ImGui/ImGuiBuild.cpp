fb51b66812eca6b70ed0e35ecba091e9874b5bea
ILLmew <illmewds@gmail.com>
2020-11-15 19:26:26 +0800

    delete Core

--- Seagull Core/src/ImGui/ImGuiBuild.cpp
+++
@@ -1,4 +0,0 @@
-#include "sgpch.h"
-
-#include "backends/imgui_impl_dx12.cpp"
-#include "backends/imgui_impl_win32.cpp"
--- Seagull Core/src/Renderer/RenderCommand.cpp
+++
@@ -1,9 +0,0 @@
-#include "sgpch.h"
-#include "RenderCommand.h"
-
-#include "Platform/DirectX/DirectX12RendererAPI.h"
-
-namespace SG
-{
-	RendererAPI* RenderCommand::s_RendererAPI = new DirectX12RendererAPI();
-}
